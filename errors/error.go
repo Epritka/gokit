bfc93d25db918f5d2fce51d22891276e52f4d723
Epritka <epritka.develop@gmail.com>
2023-05-26 12:36:37 +0300

    delete Marshal error

--- errors/error.go
+++ errors/error.go
@@ -1,7 +1,5 @@
 package errors
 
-import "encoding/json"
-
 type DefaultError struct {
 	Message     string         `json:"message,omitempty"`
 	Type        ErrorType      `json:"type"`
@@ -19,12 +17,4 @@ func (e *DefaultError) Error() string {
 	}
 
 	return "it`s default error"
-}
-
-func (e *DefaultError) MarshalJSON() ([]byte, error) {
-	if e.Message == "" {
-		e.Message = DefaultErorMessages[e.Type]
-	}
-
-	return json.Marshal(e)
 }
